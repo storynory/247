d93fcd7400b5cb021c1b4219e3b0bc0be84461b5
HUGH FRASER <hugh@storynory.com>
2019-03-31 17:09:24 +0100

    blog

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,15 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: `Gridsome Starter Blog`,
+  titleTemplate: `%s - Gridsome`,
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/*.md',
+        typeName: 'BlogPost',
+        route: '/:slug'
+      }
+    }
+  ]
 }
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,50 +1,80 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
-      </nav>
+      <g-link class="heading-link" :to="{ name: 'home' }">
+        <component :is="titleTag" class="heading">{{ config.siteName }}</component>
+      </g-link>
     </header>
     <slot/>
   </div>
 </template>
 
-<static-query>
-query {
-  metaData {
-    siteName
+<script>
+import config from '~/.temp/config.js';
+
+export default {
+  computed: {
+    titleTag () {
+      return this.$route.name === 'home' ? 'h1' : 'h6'
+    },
+    config () {
+      return config
+    }
   }
 }
-</static-query>
+</script>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
+<style lang="css">
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
+  a {
+    color: #4dba87;
+    text-decoration: none;
+  }
 
-.nav__link {
-  margin-left: 20px;
-}
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Mali', cursive;
+  }
+
+  h1 {
+    font-size: 48px;
+  }
+
+  h6 {
+    font-size: 20px;
+  }
+
+  body {
+    font-family: 'Nunito', sans-serif;
+    margin: 0;
+    font-size: 16px;
+    padding: 0;
+  }
+
+  .heading-link {
+    color: black;
+    text-decoration: none;
+  }
+
+  .layout {
+    max-width: 600px;
+    margin: 0 auto;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  .layout {
+    margin-top: 30px;
+    margin-bottom: 50px;
+  }
+
+  .heading {
+    margin-bottom: 20px;
+    font-family: 'Mali', cursive;
+    font-weight: bold;
+  }
 </style>
